4d37fee15b995d4d539a2f84c702a86e92913d1b
EC2 Default User <ec2-user@ip-172-31-18-55.us-east-2.compute.internal>
2018-06-23 11:07:24 +0000

    Make a Relationship model

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,10 @@
 class StaticPagesController < ApplicationController
+  
   def home
-    @micropost = current_user.microposts.build if signed_in?
-    @feed_items = current_user.feed.paginate(page: params[:page])
+    if signed_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
   def help
@@ -11,6 +14,6 @@ class StaticPagesController < ApplicationController
   end
   
   def contact
-  end  
-  
+  end
+
 end
